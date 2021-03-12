66bed02a03b67920f73f7ef815e960eefa12dad8
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-03-12 03:35:06 -0700

    commit 8th at 3 am in the morning

--- ngo/urls.py
+++ ngo/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('user.urls')),
+    path('events/', include('event.urls')),
 ]
