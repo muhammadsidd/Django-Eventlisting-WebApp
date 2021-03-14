65d118eda38b2a7e3cd641be5f04aa8d3b7fba2e
anas shahwan <anasshahwan@anass-MacBook-Pro.local>
2021-03-13 20:58:44 -0500

    fix some style and bugs

--- ngo/urls.py
+++ ngo/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('user.urls')),
+    path('', include('user.urls')),
     path('events/', include('event.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
