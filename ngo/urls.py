2a60356d71dd066d489ccad63102dcde03b1a091
anas shahwan <anasshahwan@anass-MacBook-Pro.local>
2021-03-13 19:15:06 -0500

    add user_view Page and event detail

--- ngo/urls.py
+++ ngo/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from user import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +29,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', views.registerPage, name="register"),
 ]
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
