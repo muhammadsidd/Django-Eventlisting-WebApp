3ddf56e3cb9d8b70d7b35b729bfbaa5e6c290d7d
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-03-13 21:34:27 -0700

    Events Updated

--- registration/urls.py
+++ registration/urls.py
@@ -5,8 +5,8 @@ from registration import views
 app_name='registration'
 urlpatterns = [
     re_path(r'^$', views.registration_detail, name='registration_detail'),
-    re_path(r'^addadult/(?P<event_id>\d+)/$', views.registration_adult_add, name='registration_add_adult'),
-    re_path(r'^addChild/(?P<event_id>\d+)/$', views.registration_child_add, name='registration_add_child'),
-    re_path(r'^removeadult/(?P<product_id>\d+)/$', views.registration_adult_remove, name='registration_remove_adult'),
-    re_path(r'^removechild/(?P<product_id>\d+)/$', views.registration_child_remove, name='registration_remove_child'),
+    re_path(r'^addadult/(?P<event_id>\d+)/$', views.registration_adult_add, name='registration_adult_add'),
+    re_path(r'^addChild/(?P<event_id>\d+)/$', views.registration_child_add, name='registration_child_add'),
+    re_path(r'^removeadult/(?P<product_id>\d+)/$', views.registration_adult_remove, name='registration_adult_remove'),
+    re_path(r'^removechild/(?P<product_id>\d+)/$', views.registration_child_remove, name='registration_child_remove'),
 ]
