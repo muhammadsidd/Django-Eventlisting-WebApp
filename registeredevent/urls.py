ba794a06b24d9d96d33a02bb6fedfd50932d4b93
anas shahwan <anasshahwan@anass-MacBook-Pro.local>
2021-03-16 16:06:58 -0400

    add confirmation page without design

--- registeredevent/urls.py
+++ registeredevent/urls.py
@@ -5,8 +5,10 @@ from registeredevent import views
 app_name='registeredevent'
 
 urlpatterns = [
-    path('<int:event_id>/<int:user_id>', views.registration_create, name='registeredevent'),
+    path('<int:event_id>/<int:user_id>/<str:event_title>/<int:child_price>/<int:adult_price>/', views.registration_create, name='registeredevent'),
     path('register_event_confirmation/<int:event_id>/<int:user_id>', views.registration_confirm, name='register_confirm'),
+    path('confirmation/<int:event_id>/<int:user_id>/', views.confirmation, name='confirmation'),
 
+    # path('sucess/', views.confirmationSucess, name='confirm_success'),
 
 ]
--- registeredevent/views.py
+++ registeredevent/views.py
@@ -11,29 +11,64 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 
-def registration_create(request, event_id, user_id):
+def registration_create(request, event_id, user_id,event_title,child_price,adult_price):
         # print(event_id)
         # print(user_id)
         
         # registration = RegisteredEvent.objects.all()
         form = RegistrationForm()
-        return render(request, 'registeredevent/register_event_confirmation.html', {'form': form,'event_id':event_id, 'user_id':user_id})
+        return render(request, 'registeredevent/register_event_confirmation.html', 
+        {'form': form,'event_id':event_id, 
+        'user_id':user_id,
+        'event_title':event_title,
+        'child_price':child_price,
+        'adult_price':adult_price,
+
+         
+         })
+def confirmationSucess(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def confirmation(request,event_id,user_id):
+    print('test')
+    
+    if request.method == "POST":   
+        form = RegistrationForm(request.POST)
+        print("Out")
+        if form.is_valid():
+            print("okkk")
+            registration = form.save(commit=False)
+    
+            registration.user_id = user_id
+            registration.event_id = event_id
+            print(registration)
+            
+            registration.save()
+            # total_amount = registration.total_value(event_id)
+            # registration.save()
+            # return render(request,"carts/home.html",{})
+            # return HttpResponseRedirect(reverse_lazy('adopcion:solicitud_listar'))
+            return HttpResponse("You Have Confirmed Your Registeration.. you will get an email sooon")
+            # return HttpResponseRedirect(reverse('registeredevent:confirm_success',))
+    
 
 def registration_confirm(request,event_id,user_id):
-    if request.method == "POST":
-        print("here")
-        print(event_id)
-
-        print(user_id)
+    if request.method == "POST":   
         form = RegistrationForm(request.POST)
         if form.is_valid():
             registration = form.save(commit=False)
-            registration.user_id = user_id
-            registration.event_id = event_id
-            print(event_id)
-            x = registration.total_value(event_id)
-            registration.save()
-            print(x)
-            return HttpResponseRedirect(reverse('event:event_list'))
+            # registration.user_id = user_id
+            # registration.event_id = event_id
+            # print(event_id)
+            total_amount = registration.total_value(event_id)
+            # registration.save()
+            # print(x)
+            return render(request, 'registeredevent/confirmation.html',
+             {"form":form,
+             "user_id":user_id,
+             "event_id":event_id,
+             "total_amount":total_amount,
+             })
+            # return HttpResponseRedirect(reverse('registeredevent:confirmation', {"user_id":user_id,"event_id":event_id},))
 
 
